765774b55c625eebb5029664d464da0d8f4c52f2
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2020-12-29 18:47:12 -0300

    Add input controller class

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+  private KeyCode runKey   = KeyCode.RightArrow;
+  private KeyCode jumpKey  = KeyCode.Space;
+  private KeyCode aimKey   = KeyCode.UpArrow;
+  private KeyCode shootKey = KeyCode.LeftShift;
+
+  void Update() {
+    SamusState.instance.isRunning.value  = Input.GetKey(runKey);
+    SamusState.instance.isAiming.value   = Input.GetKey(aimKey);
+    SamusState.instance.isShooting.value = Input.GetKey(shootKey);
+
+    SamusState.instance.jumpState.value = newJumpState(Input.GetKey(jumpKey));
+  }
+
+  private static JumpState newJumpState(bool jumpKeyPressed) {
+    if(SamusState.instance.jumpState.value == JumpState.Grounded && jumpKeyPressed) {
+      return JumpState.Jumping;
+    }
+
+    return SamusState.instance.jumpState.value;
+  }
+}
