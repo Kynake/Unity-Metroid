a9427551a50e74b627b97fc23dd9e12783e5aa7e
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2020-12-29 18:45:36 -0300

    Add new Utils class, with WatchedValue

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchedValue<T> {
+  public WatchedValue(T startingValue) {
+    _value = startingValue;
+  }
+  private System.Type type = typeof(T); // Store type to diferentiate between primitives/enums and objects
+
+  private T _value;
+  public delegate void valueChangeAction(T value);
+  public event valueChangeAction OnChange;
+  public T value {
+    get { return _value; }
+    set {
+      if(!value.Equals(_value)) { // Always same Type, test Value
+        _value = value;
+        OnChange?.Invoke(value);
+      }
+    }
+  }
+}
