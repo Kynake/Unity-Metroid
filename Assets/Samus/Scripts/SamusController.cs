c9381d95bbbb6c9ab7ae7d29d2b3fb0ebe9aa016
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2020-12-30 03:44:40 -0300

    reate Samus Controller Class

---
+++ Assets/Samus/Scripts/SamusController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SamusController : MonoBehaviour
+{
+
+  public float movementSpeed; // in tiles per second
+  public float jumpHeight; // in tiles per second
+
+//   public PlayerInput playerInput;
+
+  private Rigidbody2D _rigidbody;
+
+  private void Awake() {
+    _rigidbody = GetComponent<Rigidbody2D>();
+    if (_rigidbody == null)
+    {
+      Debug.LogError("Samus Rigidbody2D not found!");
+      return;
+    }
+  }
+
+  private void Start() {
+
+  }
+
+  private void Update() {
+
+  }
+
+  public void OnRun(InputAction.CallbackContext value) {
+    print("OnRun");
+  }
+
+  public void OnLongJump(InputAction.CallbackContext value) {
+
+    print("OnLongJump");
+  }
+
+  public void OnShortJump(InputAction.CallbackContext value) {
+
+    print("OnShortJump");
+  }
+
+  public void OnAim(InputAction.CallbackContext value) {
+
+    print("OnAim");
+  }
+
+  public void OnNormalShoot(InputAction.CallbackContext value) {
+
+    print("OnNormalShoot");
+  }
+
+  public void OnFastShoot(InputAction.CallbackContext value) {
+
+    print("OnFastShoot");
+  }
+
+  public void OnEnterMorphball(InputAction.CallbackContext value) {
+
+    print("OnOnEnterMorphballRun");
+  }
+
+  public void OnExitMorphball(InputAction.CallbackContext value) {
+
+    print("OnExitMorphball");
+  }
+
+  public void OnSwitchWeapon(InputAction.CallbackContext value) {
+
+    print("OnOnSwitchWeaponun");
+  }
+
+  public void OnPauseGame(InputAction.CallbackContext value) {
+
+    print("OnPauseGame");
+  }
+
+}
