5dff65e32a9bfa873a7b6eb4b33311baabdee9d5
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-01-02 04:04:28 -0300

    WIP detect side collisions

--- Assets/Shared/Utils.cs
+++ Assets/Shared/Utils.cs
@@ -23,4 +23,30 @@ public static class Utils {
     }
   }
 
+  public enum SideCollision {
+    None  = 0b0000,
+    Up   = 0b0001,
+    Down  = 0b0010,
+    Left  = 0b0100,
+    Right = 0b1000,
+    Walls = Left | Right,
+    All   = Up | Down | Walls
+  }
+
+  public static SideCollision boxCheckCollision(BoxCollider2D collider, float margin, LayerMask layerMask) {
+    var registeredCollisions = SideCollision.None;
+    // Up
+    registeredCollisions |= Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0, Vector2.up, margin, layerMask).collider == null? SideCollision.None : SideCollision.Up;
+
+    // Down
+    registeredCollisions |= Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0, Vector2.down, margin, layerMask).collider == null? SideCollision.None : SideCollision.Down;
+
+    // Left
+    registeredCollisions |= Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0, Vector2.left, margin, layerMask).collider == null? SideCollision.None : SideCollision.Left;
+
+    // Right
+    registeredCollisions |= Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0, Vector2.right, margin, layerMask).collider == null? SideCollision.None : SideCollision.Right;
+
+    return registeredCollisions;
+  }
 }
