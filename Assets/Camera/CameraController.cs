ef4b37ebe755b6596b48192b11d5bd5c54330e06
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-01-03 21:24:42 -0300

    Skeleton camera 'Follow Player' script

---
+++ Assets/Camera/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+  public GameObject samus;
+  public bool freezeY;
+
+  private Vector3 _holdingVector = Vector3.zero;
+
+  void Update() {
+    _holdingVector.Set(samus.transform.position.x, freezeY? transform.position.y : samus.transform.position.y, transform.position.z);
+    transform.position = _holdingVector;
+  }
+}
