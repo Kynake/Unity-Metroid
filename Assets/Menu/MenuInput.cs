68f144a8a559dd0802501180b7574af25b066471
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-01-22 04:19:44 -0300

    Add Input Handler to menu, switch scenes on press start

---
+++ Assets/Menu/MenuInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class MenuInput : MonoBehaviour {
+  public string gameSceneName;
+
+  private void OnStartGame(InputValue value) {
+    SceneManager.LoadScene(gameSceneName);
+  }
+}
